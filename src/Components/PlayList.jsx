c148de8dbd9eb5ea5828002b6db32d4d5511ec0b
Abdul-Rafey Shaikh <as05324@surrey.ac.uk>
2026-02-10 21:06:08 +0000

    Implemented PlayList.jsx component

--- src/Components/PlayList.jsx
+++ src/Components/PlayList.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Track from './Track';
 
 
 function PlayList(props) {
+    return (
+       
+        <div>
+         {props.tracks.map(track => (
+         <Track
+        key={track.trackId}
+        track={track}
+        removeTrack={props.removeTrack} />
+            )) }
 
+       </div> 
+    );
 }
 
 export default PlayList;
